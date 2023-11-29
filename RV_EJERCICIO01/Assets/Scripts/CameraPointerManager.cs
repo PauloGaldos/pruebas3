56513007c2a720e703d19dfe455c17ffe8f2c179
paulo <paulogaldos2308@gmail.com>
2023-11-28 20:16:50 -0500

    puntero seleccion
    agregar funcion al puntero

--- RV_EJERCICIO01/Assets/Scripts/CameraPointerManager.cs
+++ RV_EJERCICIO01/Assets/Scripts/CameraPointerManager.cs
@@ -15,6 +15,15 @@ public class NewBehaviourScript : MonoBehaviour
 
     private const float _maxDistance = 10;
     private GameObject _gazedAtObject = null;
+    private void Start()
+    {
+        GazeManager.Instance.OnGazeSelection += GazeSelection;
+
+    }
+    private void GazeSelection()
+    {
+        _gazedAtObject?.SendMessage("OnpointerClick", null,SendMessageOptions.DontRequireReceiver);
+    }
 
     /// <summary>
     /// Update is called once per frame.
